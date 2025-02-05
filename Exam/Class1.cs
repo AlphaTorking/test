21d93b86b876d5950d616087e127628cfd180dfc
p1-21 <p1-21@3-201-10>
2025-02-05 11:15:22 +0300

    test

---
+++ Exam/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Class1
+    {
+        public int Id = 1234;
+    }
+}
